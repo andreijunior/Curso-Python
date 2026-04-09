954fb6c8c041840bf7be72ca47c3c776b68c2942
Andrei de Oliveira Junior <145500768+andreijunior@users.noreply.github.com>
2026-04-09 16:48:08 -0300

    Implement sum script for two input numbers

---
+++ Curso-Guanabara/Mundo-1/Desafio_03.py
@@ -0,0 +1,6 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+numero_1 = int(input('Digite o primeiro número: '))
+numero_2 = int(input('Digite o segundo número: '))
+resultado = numero_1 + numero_2
+print('A soma entre o {} e {} é {}.'.format(numero_1, numero_2, resultado))
